949630d8a2b32b870847933746595834869e57f1
Komila Yapparova <lady.k.yapparova@gmail.com>
2021-10-05 01:15:41 +0300

    Updated jil

---
+++ jil.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main (String [] args ){
+		System.out.println(convert(1));
+                System.out.println("That'll be it");
+	}
+	public static int convert (int x) {
+		return 40 * x;
+	}
+}
