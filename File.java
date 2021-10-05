1c1714ae063760070589fc0695590c8d74959ba1
Komila Yapparova <lady.k.yapparova@gmail.com>
2021-10-05 21:52:34 +0300

    brnew Updated File

--- File.java
+++ File.java
@@ -1,9 +1,9 @@
 public class File {
         public static void main (String [] args ){
-            System.out.println(convert(4));
+            System.out.println(convert(8));
             System.out.println("That'll be it");
         }
         public static int convert (int x) {
-            return 20 * x;
+            return 10 * x;
         }
 }
