1b6cba7de133129ca569dc43451100a274b8fcfb
Komila Yapparova <lady.k.yapparova@gmail.com>
2021-10-05 21:44:44 +0300

    brnew Updated File

---
+++ File.java
@@ -0,0 +1,9 @@
+public class File {
+        public static void main (String [] args ){
+            System.out.println(convert(4));
+            System.out.println("That'll be it");
+        }
+        public static int convert (int x) {
+            return 20 * x;
+        }
+}
--- kol.java
+++ kol.java
@@ -1,10 +1,10 @@
 public class Main {
         public static void main (String [] args) {
                 System.out.println ("Hello");
-                System.out.println ("people");  
+                System.out.println ("people");
                 System.out.println ("Tomorrow is Friday");
                 System.out.println ("Tomorrow is not Friday");
-                System.out.rpintln ("Update - maybe Tomorrow is Friday" )
+                System.out.println ("Update - maybe Tomorrow is Friday" );
         }
 }
 
