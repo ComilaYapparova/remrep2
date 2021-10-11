fb39ad01783cb9ee235c3d35069e1af232d0553a
Komila Yapparova <lady.k.yapparova@gmail.com>
2021-10-12 00:11:42 +0300

    Modified kol.java

--- kol.java
+++ kol.java
@@ -3,7 +3,8 @@ public class Main {
                 System.out.println ("Hello");
                 System.out.println ("people");  
                 System.out.println ("Tomorrow is Friday");
-                System.out.println ("Delete all now")'
+                System.out.println ("Delete all now");
+                System.out.println ("IDK");
         }
 }
 
